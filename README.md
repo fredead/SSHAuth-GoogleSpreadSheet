da462b011bb4b0abd7b1fed7a29005c013fe0949
Simon Loader <simon.loader@r3cev.com>
2016-09-16 09:37:00 +0100

    Demo of using google spreadsheet to hold auth keys for accounts on a box  This does not mean accounts exist but is useful for providing changeable keys for a shared system

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# SSHAuth-GoogleSpreadSheet
+# What is this
+
+
+ This is an example of using google spreadsheets to hold ssh authz keys for a group of users
+
+currently it is done in this order but this can be easily changed
+
+Owner,user,hostname,key
+
+
+# How to implement
+
+
+
+## possible installs required
+
+'''
+apt-get install python-oauth2client pip
+pip install gspread
+'''
+
+set up the credetial in sshtest.py currently setup for readonly to https://docs.google.com/spreadsheets/d/1II0DzsxRgtsNBlkt-BXL16gmxbxwBirWng119ZHBPmk/edit#gid=0
+
+now install the code
+
+'''
+cp sshtest.py /usr/bin
+chown root:root /usr/bin/sshtest.py
+chmod 766 /usr/bin/sshtest.py
+'''
+
+Edit sshd_config
+'''
+vi /etc/ssh/sshd_config
+'''
+
+Add these lines 
+'''
+AuthorizedKeysCommand /usr/bin/sshtest.py
+AuthorizedKeysCommandUser nobody
+'''
+
+systemctl restart sshd
+
+And now you can login
+
+
---
+++ sshtest.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+import gspread
+import sys
+import socket
+from oauth2client.service_account import ServiceAccountCredentials
+import logging
+import logging.handlers
+
+logger = logging.getLogger('sshauthGsheet')
+logger.setLevel(logging.DEBUG)
+handler = logging.handlers.SysLogHandler(address = '/dev/log')
+logger.addHandler(handler)
+
+# Change this to the API ID you get
+jsonData = { "type": "service_account", "project_id": "sshaut", "private_key_id": "3", "private_key": "-----BEGIN PRIVATE KEY-----\n\n-----END PRIVATE KEY-----\n", "client_email": "test@ssh.gserviceaccount.com", "client_id": "127", "auth_uri": "https://accounts.google.com/o/oauth2/auth", "token_uri": "https://accounts.google.com/o/oauth2/token", "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs", "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/test%40ssh.iam.gserviceaccount.com" }
+
+scope = ['https://spreadsheets.google.com/feeds']
+credentials = ServiceAccountCredentials.from_json_keyfile_dict(jsonData,scope)
+# Or if you dont want to inline the auth which means you could keep the details more secret
+#credentials = ServiceAccountCredentials.from_json_keyfile_name('/etc/config/sshauth.json',scope)
+
+
+# Setting that define sheet and which column to use for what
+sshKeyColumn = 4
+hostColumn = 3
+userColumn = 2
+sheetName = "SSHauth"
+hostname = socket.gethostname()
+
+gc = gspread.authorize(credentials)
+worksheet = gc.open(sheetName).sheet1
+
+cellList = worksheet.findall(sys.argv[1])
+
+logger.critical('Mached %d with user "%s" testing for  host "%s"',len(cellList),sys.argv[1],hostname)
+
+
+for cell in cellList:
+  if cell.col == userColumn:
+	if hostname == worksheet.cell(cell.row,hostColumn).value:
+  		print worksheet.cell(cell.row,sshKeyColumn).value
